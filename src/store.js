2d9f7dc91e438e28b677146023a40aab8e260f28
Titus Soporan <titus@tsoporan.com>
2017-03-15 01:21:44 -0400

    Don't use dummy data

--- src/store.js
+++ src/store.js
@@ -5,41 +5,41 @@
 import { createStore } from 'redux';
 import noteReducer from './reducers/notes';
 
-import { v4 } from 'node-uuid';
+// import { v4 } from 'node-uuid';
 
 const initialState = {
-  fetchedNotes: false,
-  notes: [
-    {
-      id: v4(),
-      text: "This is some sample short text :)",
-      dateCreated: Date.now(),
-    },
+  fetchedInitial: false,
+  notes: [],
+  //  {
+  //    _id: v4(),
+  //    body: "This is some sample short text :)",
+  //    dateCreated: Date(),
+  //  },
 
-    {
-      id: v4(),
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      dateCreated: Date.now() - 100000,
-    },
+  //  {
+  //    _id: v4(),
+  //    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  //    dateCreated: Date(Date.now() - 100000),
+  //  },
 
-    {
-      id: v4(),
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud",
-      dateCreated: Date.now() - 1000000,
-    },
+  //  {
+  //    _id: v4(),
+  //    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud",
+  //    dateCreated: Date(Date.now() - 1000000),
+  //  },
 
-    {
-      id: v4(),
-      text: "ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, ",
-      dateCreated: Date.now() - 10000000,
-    },
+  //  {
+  //    _id: v4(),
+  //    body: "ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, ",
+  //    dateCreated: Date(Date.now() - 10000000),
+  //  },
 
-    {
-      id: v4(),
-      text: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.",
-      dateCreated: Date.now() - 10000000,
-    },
-  ]
+  //  {
+  //    _id: v4(),
+  //    body: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.",
+  //    dateCreated: Date(Date.now() - 10000000),
+  //  },
+  //]
 };
 
 const store = createStore(
